06d2fe1f548206d83358ad1dceb45b34eeb12930
Harsh <hs5686584@gmail.com>
2025-01-05 00:40:18 +0530

    routing part done

--- Basics/server/index.js
+++ Basics/server/index.js
@@ -1,7 +1,17 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, resp) => {
-  resp.end("I am calling from server");
+  const pathName = req.url;
+
+  if (pathName === "/overview") {
+    resp.end("i belongs to overview section");
+  } else if (pathName === "/product") {
+    resp.end("i am from product section");
+  } else {
+    resp.writeHead(777);
+    resp.end("page out break like a zombie appocalypse one");
+  }
 });
 
 server.listen(8765, "127.0.0.1", () => {
