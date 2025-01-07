9f622eb581de0f24358a0e43dcfa3a8a715e9ea7
Harsh <hs5686584@gmail.com>
2025-01-07 23:47:02 +0530

    updated api.js file with writehead and content type in headers

--- Basics/server/api.js
+++ Basics/server/api.js
@@ -1,16 +1,26 @@
 const fs = require("fs");
 const http = require("http");
+const { type } = require("os");
 const { json } = require("stream/consumers");
 const url = require("url");
 
-const apiData = fs.readFileSync("./Data.txt", "utf-8");
-const jsonData = JSON.parse(apiData);
+const textData = fs.readFileSync("./Data.txt", "utf-8");
+const apiData = fs.readFileSync("./main.json", "utf-8");
+console.log(typeof apiData);
 
 const server = http.createServer((req, resp) => {
   const path = req.url;
 
-  if (path === "/api-data") {
+  if (path === "/text-data") {
+    resp.end(textData);
+    console.log(typeof apiData);
+  } else if (path === "/api-data") {
+    resp.writeHead(200, {
+      "content-type": "application/json",
+    });
     resp.end(apiData);
+  } else {
+    resp.end("404 not found");
   }
 });
 
