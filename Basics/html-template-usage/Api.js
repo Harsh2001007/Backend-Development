24c920161c2ab9e25ea99f231dd23b3ad905fe4e
Harsh <hs5686584@gmail.com>
2025-01-11 01:17:08 +0530

    self created module part done

--- Basics/html-template-usage/Api.js
+++ Basics/html-template-usage/Api.js
@@ -1,24 +1,8 @@
 const fs = require("fs");
 const http = require("http");
-const path = require("path");
 const url = require("url");
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%PRODUCT_IMAGE%}/g, product.image);
-  output = output.replace(/{%PRODUCT_FROM%}/g, product.from);
-  output = output.replace(/{%PRODUCT_NUTRIENT%}/g, product.nutrients);
-  output = output.replace(/{%PRODUCT_QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRODUCT_PRICE%}/g, product.price);
-  output = output.replace(/{%PRODUCT_DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%PRODUCT_ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
