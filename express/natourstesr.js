2004bf5f6e38c4ebe83cc4e911d7bd426a0dbb2b
Harsh <hs5686584@gmail.com>
2025-01-27 00:47:34 +0530

    params part done on get api

--- express/natourstesr.js
+++ express/natourstesr.js
@@ -8,6 +8,7 @@ const toursData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// get api work tree
 app.get('/api/v1/tours', (req, resp) => {
   resp.status(200).json({
     status: 'success',
@@ -18,6 +19,30 @@ app.get('/api/v1/tours', (req, resp) => {
   });
 });
 
+// get api with params
+
+app.get('/api/v1/tours/:id', (req, resp) => {
+  console.log(req.params);
+  const tourId = req.params.id * 1;
+  const tour = toursData.find((item) => item.id === tourId);
+
+  if (tourId > toursData.length) {
+    console.log('401 called');
+    return resp.status(401).json({
+      status: 'failed',
+      message: 'invalid id on params',
+    });
+  }
+
+  resp.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// post api work tree
 app.post('/api/v1/tours', (req, resp) => {
   const newId = toursData[toursData.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
