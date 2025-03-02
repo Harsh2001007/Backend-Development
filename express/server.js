30fab6623beef2822da72b09e32fa22495b964c2
Harsh <hs5686584@gmail.com>
2025-03-03 00:46:44 +0530

    connected mongo db to code

--- express/server.js
+++ express/server.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connected successfully');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5556, () => {
   console.log('API started on http://localhost:5556');
