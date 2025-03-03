fa118956b0d113840ee9d115b8726186afb8870b
Harsh <hs5686584@gmail.com>
2025-03-04 00:47:21 +0530

    new data saved in DB && model refactored as per MVC

--- express/controllers/tourController.js
+++ express/controllers/tourController.js
@@ -1,19 +1,4 @@
-const fs = require('fs');
-
-const toursData = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkId = (req, resp, next, value) => {
-  console.log('middleware id value is ', value);
-  if (req.params.id * 1 > toursData.length) {
-    return resp.status(401).json({
-      status: 'failed',
-      message: 'invalid id',
-    });
-  }
-  next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, resp, next) => {
   if (!req.body.name || !req.body.price) {
@@ -28,31 +13,20 @@ exports.checkBody = (req, resp, next) => {
 exports.getAllTours = (req, resp) => {
   resp.status(200).json({
     status: 'success',
-    result: toursData.length,
-    data: {
-      toursData,
-    },
+    // result: toursData.length,
+    // data: {
+    //   toursData,
+    // },
   });
 };
 
 exports.postTour = (req, resp) => {
-  const newId = toursData[toursData.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  toursData.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(toursData),
-    (err) => {
-      resp.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  resp.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.patchTour = (req, resp) => {
@@ -73,11 +47,11 @@ exports.deleteTour = (req, resp) => {
 
 exports.getParticularTour = (req, resp) => {
   const tourId = req.params.id * 1;
-  const tour = toursData.find((item) => item.id === tourId);
-  resp.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const tour = toursData.find((item) => item.id === tourId);
+  // resp.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
--- express/server.js
+++ express/server.js
@@ -15,12 +15,23 @@ mongoose
     useFindAndModify: false,
   })
   .then((con) => {
-    console.log(con.connections);
+    // console.log(con.connections);
     console.log('DB connected successfully');
   })
   .catch((err) => {
     console.log(err);
   });
+
+// const testTour = new Tour({
+//   name: 'The Forest Hiker 3',
+//   rating: 4.9,
+//   price: 510,
+// });
+
+// testTour
+//   .save()
+//   .then((doc) => console.log(doc))
+//   .catch((err) => console.log('ERROR :', err));
 
 app.listen(5556, () => {
   console.log('API started on http://localhost:5556');
