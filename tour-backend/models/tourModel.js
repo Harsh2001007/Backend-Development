20c0ae98fec3da0d27da83b0d7457e7ee08674fd
Harsh <hs5686584@gmail.com>
2025-03-12 23:55:51 +0530

    updated mongoose schema

--- tour-backend/models/tourModel.js
+++ tour-backend/models/tourModel.js
@@ -6,6 +6,53 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, "duration is required"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "max group size is required"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "difficulty is required"],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "summary is required"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "image cover url is required"],
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: {
+    type: [Date],
+  },
+  priceDiscount: {
+    type: Number,
+  },
+
   rating: {
     type: Number,
     default: 4.0,
