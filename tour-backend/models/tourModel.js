9a3fad7b1722f17d19a43e6014de63eae8a1deec
Harsh <hs5686584@gmail.com>
2025-03-22 23:08:50 +0530

    files updated

--- tour-backend/models/tourModel.js
+++ tour-backend/models/tourModel.js
@@ -59,7 +59,7 @@ const tourSchema = new mongoose.Schema({
   },
   price: {
     type: Number,
-    required: [true, "A tour must have a price"],
+    required: [true, "A tour must have price"],
   },
 });
 
