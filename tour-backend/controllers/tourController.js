6163e643b534a5317610f6dee8c7b03c0b255bea
Harsh <hs5686584@gmail.com>
2025-03-26 01:00:07 +0530

    added class methods and refactored the code

--- tour-backend/controllers/tourController.js
+++ tour-backend/controllers/tourController.js
@@ -62,48 +62,6 @@ class APIFeatures {
 
 exports.getAllTour = async (req, resp) => {
   try {
-    // const queryObj = { ...req.query };
-
-    // const excludeFields = ["page", "sort", "limit", "fields"];
-    // excludeFields.forEach((el) => delete queryObj[el]);
-    // console.log(req.query, queryObj);
-
-    // 1.  Advance filtering
-
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr), typeof queryStr);
-
-    // let query = Tour.find(JSON.parse(queryStr));
-
-    // 2.  Sorting
-
-    // if (req.query.sort) {
-    //   query = query.sort(req.query.sort);
-    // }
-
-    // 3.  Field limiting
-
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(",").join(" ");
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select("-__v");
-    // }
-
-    // 4. Pagination
-
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) throw new Error("This page does not exist");
-    // }
-
     // Executing Query
 
     const features = new APIFeatures(Tour.find(), req.query)
