028ace9e1d9bd66ed4996f326b47f1b698be6aee
Harsh <hs5686584@gmail.com>
2025-04-02 02:05:27 +0530

    created global express error handling middleware

--- tour-backend/app.js
+++ tour-backend/app.js
@@ -7,4 +7,28 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1/tours", tourRoutes);
 
+app.all("*", (req, resp, next) => {
+  //   resp.status(404).json({
+  //     status: "fail",
+  //     message: `can't find ${req.originalUrl} on this server`,
+  //   });
+
+  const err = new Error(`can't find ${req.originalUrl} on this server`);
+  (err.status = "fail"), (err.statusCode = 404);
+
+  next(err);
+});
+
+// express error middleware
+
+app.use((err, req, resp, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  resp.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
