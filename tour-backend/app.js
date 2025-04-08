301ae45932fecc34629f276291fe292bfcd499f9
Harsh <hs5686584@gmail.com>
2025-04-08 23:52:20 +0530

    error handling part done

--- tour-backend/app.js
+++ tour-backend/app.js
@@ -1,34 +1,30 @@
 const express = require("express");
 const tourRoutes = require("./routes/tourRoutes");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const userRouter = require("./routes/userRoutes");
+
 const app = express();
 
-// Middle wares
+// 1) MIDDLEWARES
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
 app.use("/api/v1/tours", tourRoutes);
+app.use("/api/v1/users", userRouter);
 
-app.all("*", (req, resp, next) => {
-  //   resp.status(404).json({
-  //     status: "fail",
-  //     message: `can't find ${req.originalUrl} on this server`,
-  //   });
-
-  const err = new Error(`can't find ${req.originalUrl} on this server`);
-  (err.status = "fail"), (err.statusCode = 404);
-
-  next(err);
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-// express error middleware
-
-app.use((err, req, resp, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  resp.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
