7ff1585865efeaf744d13c5cffc8db1b6f96bbb9
Harsh <hs5686584@gmail.com>
2025-04-15 00:22:46 +0530

    jwt sign in part done

--- tour-backend/controllers/authController.js
+++ tour-backend/controllers/authController.js
@@ -1,8 +1,18 @@
+const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsynch");
 
 exports.signUp = catchAsync(async (req, resp, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   resp.status(201).json({
     status: "success",
